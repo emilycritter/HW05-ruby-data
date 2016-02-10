c8963882a00d8c0dfb5ba04ed4479a11a39aa467
emily.c.ritter <emily.c.ritter@gmail.com>
2016-02-10 14:34:40 -0600

    Remove 'words' array, and calculate number of 5-letter words in puts statement.

--- part1.rb
+++ part1.rb
@@ -3,8 +3,4 @@ sentence = "Tacos must not have lettuce Only meat and cheese and salsa"
 sentence_ary = sentence.split
 puts "The sentence as an array: #{sentence_ary.inspect}"
 
-words = sentence_ary.select do |word|
-  word.length == 5
-end
-
-puts "There are #{words.length} words with 5 letters."
+puts "There are #{(sentence_ary.select {|word| word.length == 5}).count} words with 5 letters."
