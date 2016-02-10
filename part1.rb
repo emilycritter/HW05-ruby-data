0fdda66c6147870a32f91e029b6cf46499010d02
emily.c.ritter <emily.c.ritter@gmail.com>
2016-02-10 13:49:01 -0600

    5 letter words, not words greater than 5 letters.

--- part1.rb
+++ part1.rb
@@ -4,7 +4,7 @@ sentence_ary = sentence.split
 puts "The sentence as an array: #{sentence_ary.inspect}"
 
 words = sentence_ary.select do |word|
-  word.length > 5
+  word.length == 5
 end
 
 puts "There are #{words.length} words with 5 letters."
